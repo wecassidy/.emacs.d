933573859ce82216362d9f71e7ca4c063148a15c
wecassidy <wec.notif.github@gmail.com>
2020-01-16 14:45:12 -0800

    Auto blacken code, company something something

--- config/modes/wec-py.el
+++ config/modes/wec-py.el
@@ -17,19 +17,22 @@
 
 ;; Autocomplete
 (require 'company)
-;(add-hook 'python-mode-hook (lambda () (add-to-list 'company-backends 'company-jedi)))
+(add-hook 'python-mode-hook (lambda () (add-to-list 'company-backends 'company-jedi)))
 
 ;; Execute Pylint with Python 3
 (require 'flycheck-pycheckers)
-;(with-eval-after-load 'flycheck
-; (add-hook 'flycheck-mode-hook #'flycheck-pycheckers-setup)
-; (setq flycheck-pycheckers-checkers '(pep8)))
-; (setq flycheck-python-pylint-executable "/usr/bin/python3")
+(with-eval-after-load 'flycheck
+  (add-hook 'flycheck-mode-hook #'flycheck-pycheckers-setup)
+  (setq flycheck-pycheckers-checkers '(pylint)))
+  (setq flycheck-python-pylint-executable "/usr/bin/python3")
 
 ;; Show source code outline tree with C-c x
 (eval-after-load "python"
   '(define-key python-mode-map "\C-cx" 'jedi-direx:switch-to-buffer))
 (add-hook 'jedi-mode-hook 'jedi-direx:setup)
+
+;; Automatically blacken code
+(add-hook 'python-mode-hook 'blacken-mode)
 
 (provide 'wec-py)
 ;;; wec-py.el ends here

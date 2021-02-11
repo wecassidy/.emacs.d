44c29f1ee39c8b6a1f73a0621d93df6cf7d39d22
wecassidy <wec.notif.github@gmail.com>
2021-02-11 10:20:05 -0500

    Delete custom settings from init.el
    better-defaults uses custom.el instead of cluttering init.el

--- custom.el
+++ custom.el
@@ -4,10 +4,26 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("aaa4c36ce00e572784d424554dcc9641c82d1155370770e231e10c649b59a074" "08a27c4cde8fcbb2869d71fdc9fa47ab7e4d31c27d40d59bf05729c4640ce834" "f4876796ef5ee9c82b125a096a590c9891cec31320569fc6ff602ff99ed73dca" default)))
+   '("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default))
+ '(package-selected-packages
+   '(undo-tree blacken magit flycheck-status-emoji company-statistics flycheck-irony platformio-mode nlinum sr-speedbar company-arduino company-reftex company-web mines shift-number ido-describe-bindings ido-vertical-mode flx-ido smex cdlatex auto-package-update))
+ '(paradox-github-token t)
+ '(safe-local-variable-values
+   '((TeX-command-extra-options . "-shell-escape")
+     (dired-omit-files . "^\\(?:\\.adobe\\|\\.aspell\\(?:.\\|
+\\)+\\|\\.bash\\(?:.\\|
+\\)+\\|\\.cache\\|\\.cups\\|\\.dropbox\\(?:.\\|
+\\)*\\|\\.electron\\|\\.esd_auth\\|\\.fonts\\|\\.gnupg\\|\\.gphoto\\|\\.hplip\\|\\.ICEauthority\\|\\.ipython\\|\\.lesshst\\|\\.local\\|\\.macromedia\\|\\.mozilla\\|\\.node-gyp\\|\\.npm\\|\\.pki\\|\\.pylint\\.d\\|\\.python_history\\|\\.wget-hsts\\|\\.xournal\\|\\.zcompdump\\(?:.\\|
+\\)*\\|\\.zenmap\\)$")))
+ '(send-mail-function 'smtpmail-send-it))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(company-preview ((t (:foreground "#839496" :background "#002b36" :underline t))))
+ '(company-scrollbar-bg ((t (:background "#657b83"))))
+ '(company-scrollbar-fg ((t (:background "#93a1a1"))))
+ '(company-tooltip ((t (:inherit default :background "#073642"))))
+ '(company-tooltip-common ((t (:foreground "#268bd2"))))
+ '(company-tooltip-selection ((t (:inherit font-lock-function-name-face)))))
--- init.el
+++ init.el
@@ -79,39 +79,6 @@
 (require 'wec-text)
 (require 'wec-web)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("4aee8551b53a43a883cb0b7f3255d6859d766b6c5e14bcb01bed572fcbef4328" "4cf3221feff536e2b3385209e9b9dc4c2e0818a69a1cdb4b522756bcdf4e00a4" default)))
- '(package-selected-packages
-   (quote
-    (blacken magit flycheck-status-emoji company-statistics flycheck-irony platformio-mode nlinum sr-speedbar company-arduino company-reftex company-web mines shift-number ido-describe-bindings ido-vertical-mode flx-ido smex cdlatex auto-package-update)))
- '(paradox-github-token t)
- '(safe-local-variable-values
-   (quote
-    ((TeX-command-extra-options . "-shell-escape")
-     (dired-omit-files . "^\\(?:\\.adobe\\|\\.aspell\\(?:.\\|
-\\)+\\|\\.bash\\(?:.\\|
-\\)+\\|\\.cache\\|\\.cups\\|\\.dropbox\\(?:.\\|
-\\)*\\|\\.electron\\|\\.esd_auth\\|\\.fonts\\|\\.gnupg\\|\\.gphoto\\|\\.hplip\\|\\.ICEauthority\\|\\.ipython\\|\\.lesshst\\|\\.local\\|\\.macromedia\\|\\.mozilla\\|\\.node-gyp\\|\\.npm\\|\\.pki\\|\\.pylint\\.d\\|\\.python_history\\|\\.wget-hsts\\|\\.xournal\\|\\.zcompdump\\(?:.\\|
-\\)*\\|\\.zenmap\\)$"))))
- '(send-mail-function (quote smtpmail-send-it)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(company-preview ((t (:foreground "#839496" :background "#002b36" :underline t))))
- '(company-scrollbar-bg ((t (:background "#657b83"))))
- '(company-scrollbar-fg ((t (:background "#93a1a1"))))
- '(company-tooltip ((t (:inherit default :background "#073642"))))
- '(company-tooltip-common ((t (:foreground "#268bd2"))))
- '(company-tooltip-selection ((t (:inherit font-lock-function-name-face)))))
-
 (put 'upcase-region 'disabled nil)
 
 ;; Make gc pauses faster by decreasing the threshold.

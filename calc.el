7e95f00b4dbb95ef0b75b6b38b68e07dcc7ba9f7
wecassidy <wec.notif.github@gmail.com>
2021-02-11 04:23:08 -0500

    Files autogenerated by emacs

---
+++ calc.el
@@ -0,0 +1,3 @@
+
+;;; Mode settings stored by Calc on Thu Jan 31 00:53:24 2019
+;;; End of mode settings
---
+++ custom.el
@@ -0,0 +1,13 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("aaa4c36ce00e572784d424554dcc9641c82d1155370770e231e10c649b59a074" "08a27c4cde8fcbb2869d71fdc9fa47ab7e4d31c27d40d59bf05729c4640ce834" "f4876796ef5ee9c82b125a096a590c9891cec31320569fc6ff602ff99ed73dca" default)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
